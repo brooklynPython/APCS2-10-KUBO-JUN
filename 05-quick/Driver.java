0140a8800e4f3088cf835c15bdbc40332f80562a
junkubo <73146481+junkubo@users.noreply.github.com>
2021-03-11 12:31:45 -0500

    test

---
+++ 05-quick/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+  public static void main (String args[]) {
+    int[] data = {999,999,999,4,3,2,1,0,999,999,999};
+    int start = 3;
+    int end = 7;
+    Preliminary pre = new Preliminary();
+    System.out.println(pre.partition(data, start, end));
+
+  }
+}
