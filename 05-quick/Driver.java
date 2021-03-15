a0178d8eb5562601d6961b2ea1263b880ed87bfe
brooklynPython <kuboken1@gmail.com>
2021-03-15 00:58:25 -0400

    runs but answers are wrong!

--- 05-quick/Driver.java
+++ 05-quick/Driver.java
@@ -1,16 +1,19 @@
 public class Driver {
   public static void main (String args[]) {
-    int[] data = {999,999,999,4,3,2,1,0,999,999,999};
+    int[] data = {1,10,15,2,54,3,66,88,12};
     int start = 3;
     int end = 7;
     Preliminary pre = new Preliminary();
     System.out.println(pre.partition(data, start, end));
 
 
+    System.out.println("====  Quick Select ====");
     Quick quick = new Quick();
-    //int x = quick.quickselect(data, 5);
-    //System.out.println(x);
+    int x = quick.quickselect(data, 3);
+    System.out.println("Quick Select Answer is: " + x);
 
+    
+    System.out.println("==== QUick Sort ====");
     quick.quicksort(data);
 
     for (int i = 0; i < data.length; i++) {
--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class Quick {
   public static int partition (int [] data, int start, int end){
+    if (start == end) return start;
+    if (data.length == 1) return start;
     Random rand = new Random();
     System.out.println("end and start is: " + end + " " + start);
     int index = rand.nextInt(end-start) + start;
@@ -13,9 +15,9 @@ public class Quick {
     int j = end;
     System.out.println(" ======  i = " + i + " j = " + j + " data length = " + data.length);
     while (i < j) {
-      System.out.println("i = " + i + " j = " + j);
-      for (i++; data[i] < pivot; i++);
-      for (j--; data[j] > pivot; j--);
+      System.out.println("i = " + i + " j = " + j + "pivot: " + pivot);
+      for (i++; i < data.length && data[i] < pivot; i++);
+      for (j--;  j >= 0 && data[j] > pivot; j--);
       if (i < j) {
         int temp = data[i];
         data[i] = data[j];
@@ -25,21 +27,19 @@ public class Quick {
 
     return j;
   }
-  public static int quickselect(int[] data, int k){
-    System.out.println("===================================");
-    int partition = partition(data, 0, data.length-1);
-    System.out.println("quickselect: " + partition + " " + k + " data.length: " + data.length );
-    System.out.println("partition: " + partition + " k: " + k);
-    if (partition == k) {
-      return data[partition];
-    }
-    else if (partition > k) {
-      return quickselect(Arrays.copyOfRange(data,0,partition), k);
-    }
-    else  {
-      return quickselect(Arrays.copyOfRange(data,partition,data.length-1), k);
-    }
 
+  public static int quickselect(int[] data, int k){
+    int low = 0;
+    int high = data.length - 1;
+    int index = k;
+
+    while (low < high) {
+        int pv = partition(data, low, high);
+        if (pv <= index) low = pv + 1;
+        else if (pv > index) high = pv -1;
+        else return data[pv];
+    }
+    return data[low];
   }
 
   public static void quicksort(int[] data){
